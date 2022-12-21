c294dc41f741dc910b3bc4cf454f9ce94f722b93
Ivan Sabater <ivansabater@gmail.com>
2022-12-21 16:16:30 +0100

    1.2.1

--- holaMundo.py
+++ holaMundo.py
@@ -1,3 +1,9 @@
+class Usuario: 
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+        
 name = input("¿Cual es tu nombre?: ")
 print("Hola Mundo " + name)
 age = input("¿Cual es tu edad?: ")
@@ -5,3 +11,5 @@ if (int(age) >= 18):
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
+usuario = Usuario(name,age)
+print(usuario)
