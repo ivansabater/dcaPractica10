12f2449760ddc8e6b52a67ca9e4d3c1878b1cd09
Ivan Sabater <ivansabater@gmail.com>
2022-12-21 16:22:49 +0100

    1.2.5

--- holaMundo.py
+++ holaMundo.py
@@ -6,20 +6,20 @@ class Usuario:
         return self.nombre
     def getEdad(self):
         return self.edad
+    def mayorEdad(self):
+        if (int(self.edad) >= 21):
+            return "Eres mayor de edad"
+        else:
+            return "Eres menor de edad"
 
 anyadirUsuarios = True
 listaUsuarios = list()
 while(anyadirUsuarios):
     name = input("¿Cual es tu nombre?: ")
-    print("Hola Mundo " + name)
     age = input("¿Cual es tu edad?: ")
-    if (int(age) >= 21):
-        print("Eres mayor de edad")
-    else:
-        print("Eres menor de edad")
     listaUsuarios.append(Usuario(name,age))
     i = 1
     for usuario in listaUsuarios:
-        print("Usuario " + str(i) + ": \n nombre:" + usuario.getNombre() + ", edad: " + usuario.getEdad())
+        print("Usuario " + str(i) + ": \n nombre: " + usuario.getNombre() + ", edad: " + usuario.getEdad() + ", " + usuario.mayorEdad())
         i += 1
     anyadirUsuarios = input("¿Quieres seguir añadiendo usuarios?: ")
